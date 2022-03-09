aff18ebfb8efb365b95274eb0d0ecca77fad95da
rehabmahmoud20 <rehabgendy16@gmail.com>
2022-03-09 12:51:48 +0200

    second commit

--- src/app/portofolio/portofolio.component.ts
+++ src/app/portofolio/portofolio.component.ts
@@ -1,8 +1,7 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Content } from './../content';
 // import { Component, OnInit } from '@angular/core';
-Component
-
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-portofolio',
@@ -10,21 +9,20 @@ Component
   styleUrls: ['./portofolio.component.css'],
 })
 export class PortofolioComponent implements OnInit {
- 
   constructor() {}
-  selectedItem: Content = {image:'',title:''}; //FIXME
-  
+  selectedItem: Content = { image: '', title: '' }; //FIXME
+
   items: Content[] = [
     { image: './assets/images/cabin.png', title: 'cabin' },
-    { image: './assets/images/cake.png', title: 'cake'},
-    { image: './assets/images/circus.png', title: 'circus'},
-    { image: './assets/images/game.png', title: 'game'},
-    { image: './assets/images/safe.png', title: 'safe'},
-    { image: './assets/images/submarine.png', title: 'submarine'},
+    { image: './assets/images/cake.png', title: 'cake' },
+    { image: './assets/images/circus.png', title: 'circus' },
+    { image: './assets/images/game.png', title: 'game' },
+    { image: './assets/images/safe.png', title: 'safe' },
+    { image: './assets/images/submarine.png', title: 'submarine' },
   ];
-  // items[1].value 
+  // items[1].value
   // private readonly DEFAULT_LANGUAGE =this.items[0]
-  
+
   // this.selectedItem = this.items[0] //FIXME
 
   ngOnInit(): void {}
@@ -32,12 +30,14 @@ export class PortofolioComponent implements OnInit {
   openModal(i: number) {
     // this.items[i];
     this.selectedItem = this.items[i];
+    var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+    myModal.toggle();
     // let bootstrap open the model
     // let x:any =data-bs-toggle
     //FIXME
     // let myModal:any = document.getElementById('exampleModal')
     // let myInput:any = document.querySelector('.overlay')
-    
+
     // myModal.addEventListener('shown.bs.modal', function () {
     //   myInput.mousemove()
     // })
